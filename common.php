4796b3556babf1fe16c5186c90f77b95d8053127
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-07-23 10:21:30 -0400

    Splits up author and github functions

--- common.php
+++ common.php
@@ -29,3 +29,32 @@ function npm_curl_json($url) {
 function npm_github_link_html($link) {
   return "<a href='https://github.com/$link'>$link</a>";
 }
+
+function npm_get_github_info($post_id) {
+  $post_id = $post_id ?: get_the_ID();
+  $cache_key = "npm_github_info_$post_id";
+  $github_info = wp_cache_get($cache_key);
+
+  if($github_info !== FALSE) {
+    return $github_info;
+  }
+
+  $github = get_field('module_github', $post_id);
+
+  if(empty($github)) {
+    return null;
+  }
+
+  $github = substr($github, 0, strpos($github, '/'));
+  $github_info = npm_curl_json("https://api.github.com/users/$github");
+
+  wp_cache_set($cache_key, $github_info);
+
+  return $github_info;
+}
+
+function npm_get_github_field($field_name, $post_id) {
+  $github_info = npm_get_github_info($post_id);
+  return $github_info[$field_name];
+}
+
