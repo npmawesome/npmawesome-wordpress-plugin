658e90bd527d817342623b44535f8efab23c19ff
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-07-24 18:52:33 -0700

    Adds github repo uri

--- npmawesome.php
+++ npmawesome.php
@@ -1,12 +1,14 @@
 <?php
 /*
-Plugin Name: npmawesome
-Plugin Script: npmawesome.php
-Plugin URI: http://npmawesome.com
-Description: custom NPMAWESOME stuff
-Version: 1.0
-Author: Alex Gorbatchev
-Author URI: https://github.com/alexgorbatchev
+Plugin Name       : npmawesome
+Plugin Script     : npmawesome.php
+Plugin URI        : http://npmawesome.com
+Description       : custom NPMAWESOME stuff
+Version           : 1.0
+Author            : Alex Gorbatchev
+Author URI        : https://github.com/alexgorbatchev
+GitHub Plugin URI : npmawesome/npmawesome-wordpress-plugin
+GitHub Branch     : master
 
 === DEPENDENCIES ===
 http://php.net/manual/en/yaml.installation.php
@@ -19,7 +21,7 @@ sudo /etc/init.d/apache2 restart
 
 define('NPM_DIR', dirname(__FILE__));
 
-require_once( join( DIRECTORY_SEPARATOR, array( NPM_DIR, 'common.php' ) ) );
-require_once( join( DIRECTORY_SEPARATOR, array( NPM_DIR, 'module.php' ) ) );
-require_once( join( DIRECTORY_SEPARATOR, array( NPM_DIR, 'author.php' ) ) );
-require_once( join( DIRECTORY_SEPARATOR, array( NPM_DIR, 'partner.php' ) ) );
+require_once(join(DIRECTORY_SEPARATOR, array(NPM_DIR, 'common.php')));
+require_once(join(DIRECTORY_SEPARATOR, array(NPM_DIR, 'module.php')));
+require_once(join(DIRECTORY_SEPARATOR, array(NPM_DIR, 'author.php')));
+require_once(join(DIRECTORY_SEPARATOR, array(NPM_DIR, 'partner.php')));
